0b0bc59919d18ae96833b142d1691b556d682a40
Felipe Santos <felipe-fontes@hotmail.com>
2021-03-01 03:38:34 -0300

    Posts DataSource Fake API

---
+++ interviewapp/lib/modules/posts/datasources/post_ds_fake_api_impl.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/widgets.dart';
+import 'package:interviewapp/modules/posts/infra/contracts/post_datasource.dart';
+import 'package:interviewapp/modules/posts/infra/models/post_model.dart';
+import 'package:interviewapp/modules/users/infra/models/user_model.dart';
+
+class PostDataSourceImpl implements PostDataSource {
+  List<PostModel> _db = <PostModel>[
+    PostModel(
+        id: UniqueKey().toString(),
+        user: UserModel(id: UniqueKey().toString()),
+        date: DateTime.now(),
+        message:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard'),
+    PostModel(
+        id: UniqueKey().toString(),
+        user: UserModel(id: UniqueKey().toString()),
+        date: DateTime.now(),
+        message:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard'),
+    PostModel(
+        id: UniqueKey().toString(),
+        user: UserModel(id: UniqueKey().toString()),
+        date: DateTime.now(),
+        message:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard'),
+    PostModel(
+        id: UniqueKey().toString(),
+        user: UserModel(id: UniqueKey().toString()),
+        date: DateTime.now(),
+        message:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard'),
+    PostModel(
+        id: UniqueKey().toString(),
+        user: UserModel(id: UniqueKey().toString()),
+        date: DateTime.now(),
+        message:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard'),
+    PostModel(
+        id: UniqueKey().toString(),
+        user: UserModel(id: UniqueKey().toString()),
+        date: DateTime.now(),
+        message:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard')
+  ];
+
+  @override
+  Future<PostModel> add(String message, String userId, DateTime date) {
+    final post = PostModel(
+        id: UniqueKey().toString(),
+        user: UserModel(id: userId),
+        date: date,
+        message: message);
+
+    _db.add(post);
+  }
+
+  @override
+  Future<bool> delete(String postId) {
+    _db.removeWhere((x) => x.id == postId);
+    return Future.value(true);
+  }
+
+  @override
+  Future<List<PostModel>> getAll() {
+    return Future.value(_db);
+  }
+
+  @override
+  Future<PostModel> update(String postId, String message, DateTime date) {
+    final post = _db.firstWhere((x) => x.id == postId);
+    if (post != null) {
+      final newPost =
+          PostModel(id: postId, user: post.user, date: date, message: message);
+      return Future.value(newPost);
+    }
+    return Future.value(null);
+  }
+}
--- interviewapp/lib/modules/posts/datasources/post_ds_impl.dart
+++
@@ -1,3 +0,0 @@
-import 'package:interviewapp/modules/posts/infra/contracts/post_datasource.dart';
-
-abstract class PostDataSourceImpl implements PostDataSource {}
