b1c585f15d9a873554251a17bd41d2e28fecbea2
Felipe Santos <felipe.santos@tg4.com.br>
2021-03-04 04:27:40 -0300

    custom gradientcard widget

--- interviewapp/lib/pages/home/subpages/post/posts_page.dart
+++ interviewapp/lib/pages/home/subpages/post/posts_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 import 'package:interviewapp/pages/home/subpages/post/posts_controller.dart';
 import 'package:interviewapp/shared/utils/colors.dart';
+import 'package:interviewapp/widgets/gradient_card.dart';
 import 'package:timeago/timeago.dart' as timeago;
 
 class PostsPage extends StatefulWidget {
@@ -27,66 +28,64 @@ class _PostsPageState extends State<PostsPage> {
     return Padding(
       padding: const EdgeInsets.only(bottom: 10),
       child: Observer(builder: (_) {
-        return ListView.builder(
+        return ListView.separated(
           itemCount: _postController.posts.length,
           padding: EdgeInsets.only(bottom: 16, left: 8, right: 8),
-          itemBuilder: (context, i) => Card(
-            elevation: 5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  colors: [
-                    AppColors.companyColor,
-                    AppColors.companyColorSmoth,
-                  ],
-                ),
-              ),
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        _postController.posts[i].user.name,
-                        style: TextStyle(
-                          color: AppColors.background,
-                          fontSize: 14,
-                        ),
-                      ),
-                      Spacer(
-                        flex: 1,
-                      ),
-                      Text(
-                        timeago.format(_postController.posts[i].date),
-                        style: TextStyle(
-                          color: AppColors.background,
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    _postController.posts[i].message,
-                    style: TextStyle(
-                      color: AppColors.background,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          separatorBuilder: (c, i) => SizedBox(
+            height: 20,
+          ),
+          itemBuilder: (context, i) => GradientCard(
+            startColor: AppColors.companyColor,
+            endColor: AppColors.companyColorSmoth,
+            shadowColor: AppColors.companyColor.withOpacity(0.9),
+            bolderRadius: 10,
+            child: buildCardContent(i),
           ),
         );
       }),
+    );
+  }
+
+  Container buildCardContent(int i) {
+    return Container(
+      padding: EdgeInsets.all(
+        10,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                _postController.posts[i].user.name,
+                style: TextStyle(
+                  color: AppColors.background,
+                  fontSize: 14,
+                ),
+              ),
+              Spacer(
+                flex: 1,
+              ),
+              Text(
+                timeago.format(_postController.posts[i].date),
+                style: TextStyle(
+                  color: AppColors.background,
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text(
+            _postController.posts[i].message,
+            style: TextStyle(
+              color: AppColors.background,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ interviewapp/lib/widgets/gradient_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class GradientCard extends StatelessWidget {
+  final Color startColor;
+  final Color endColor;
+  final Color shadowColor;
+  final Widget child;
+  double bolderRadius;
+  Alignment gradientStart;
+  Alignment gradientEnd;
+  double blurRadius;
+  Offset shadowOffset;
+
+  GradientCard({
+    Key key,
+    @required this.startColor,
+    @required this.endColor,
+    @required this.shadowColor,
+    @required this.child,
+    this.bolderRadius = 24,
+    this.gradientStart = Alignment.topLeft,
+    this.gradientEnd = Alignment.bottomRight,
+    this.blurRadius = 12,
+    this.shadowOffset = const Offset(0, 6),
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(this.bolderRadius),
+        gradient: LinearGradient(
+            colors: [this.startColor, this.endColor],
+            begin: this.gradientStart,
+            end: this.gradientEnd),
+        boxShadow: [
+          BoxShadow(
+            color: this.shadowColor,
+            blurRadius: this.blurRadius,
+            offset: this.shadowOffset,
+          ),
+        ],
+      ),
+      child: child,
+    );
+  }
+}
